f333595d52de23ef9611ff0d91413e22b715c94b
lutfor779 <mdlrrahman779@gmail.com>
2021-11-12 17:00:55 +0600

    delete api done

--- index.js
+++ index.js
@@ -84,6 +84,15 @@ async function run() {
             res.json(result);
         })
 
+        // delete order api
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            console.log('product deleted');
+            res.json(result);
+        })
+
         // check admin api
         app.get('/users/:email', async (req, res) => {
             const email = req.params.email;
