16504cecca0f8fdf3baf3614c4ef4ed8ec020258
lutfor779 <mdlrrahman779@gmail.com>
2021-11-13 20:28:10 +0600

    check git working

--- index.js
+++ index.js
@@ -152,13 +152,13 @@ async function run() {
         })
 
         // delete order api
-        app.delete('/orders/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const result = await ordersCollection.deleteOne(query);
-            console.log('order deleted');
-            res.json(result);
-        })
+        // app.delete('/orders/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await ordersCollection.deleteOne(query);
+        //     console.log('order deleted');
+        //     res.json(result);
+        // })
 
 
 
