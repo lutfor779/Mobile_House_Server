fef68a0463d9b8728c254c472f4b0741473a1e1c
lutfor779 <mdlrrahman779@gmail.com>
2021-11-13 20:29:47 +0600

    modified chek

--- index.js
+++ index.js
@@ -151,14 +151,14 @@ async function run() {
             res.json(result);
         })
 
-        // delete order api
-        // app.delete('/orders/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const result = await ordersCollection.deleteOne(query);
-        //     console.log('order deleted');
-        //     res.json(result);
-        // })
+        delete order api
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await ordersCollection.deleteOne(query);
+            console.log('order deleted');
+            res.json(result);
+        })
 
 
 
