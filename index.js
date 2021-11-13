317850fdce8c2f46fe72aa650333804692a11207
lutfor779 <mdlrrahman779@gmail.com>
2021-11-13 20:31:10 +0600

    handle error

--- index.js
+++ index.js
@@ -151,7 +151,7 @@ async function run() {
             res.json(result);
         })
 
-        delete order api
+        // delete order api
         app.delete('/orders/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
